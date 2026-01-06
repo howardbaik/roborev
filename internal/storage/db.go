1bf93a41e2474fcddc6b4e8817a62d1d37d9377f
Wes McKinney <wesmckinn+git@gmail.com>
2026-01-05 18:42:29 -0600

    Switch to pure-Go SQLite driver for static binaries

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 const schema = `
@@ -83,7 +83,7 @@ func Open(dbPath string) (*DB, error) {
 	}
 
 	// Open with WAL mode and busy timeout
-	db, err := sql.Open("sqlite3", dbPath+"?_journal_mode=WAL&_busy_timeout=5000")
+	db, err := sql.Open("sqlite", dbPath+"?_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)")
 	if err != nil {
 		return nil, fmt.Errorf("open database: %w", err)
 	}
